925d5fbf2b2774cf66bf60ec49761fa8df867b9b
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-04-17 12:26:16 -0400

    moved html into index.html

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import { createServer } from 'node:http'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
 
 const app = express()
 const port = 3100
 const server = createServer(app)
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 app.get('/', (req, res)=> {
-    res.send('<h1>Ahoy matey</h1>')
+    // res.send('<h1>Ahoy matey</h1>')
+    res.sendFile(join(__dirname, 'index.html'))
 })
 
 server.listen(port, () => {
