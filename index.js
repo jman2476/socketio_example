23f1c113de8966a2b1add2695a9444c20d001078
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-04-18 12:56:57 -0400

    added at-least-once message delivery

--- index.js
+++ index.js
@@ -35,19 +35,24 @@ app.get('/', (req, res)=> {
 })
 
 io.on('connection', async (socket) => {
-    console.log('a user connected')
+    console.log('a user connected', socket.id)
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
-    socket.on('chat message', async (msg) => {
+    socket.on('chat message', async (msg, clientOffset, callback) => {
         let result
         try {
-            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg)
+            result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset)
         } catch (e) {
-            
+            if (e.errno === 19){
+                callback()
+            } else {}
+
+            return
         }
 
         io.emit('chat message', msg, result.lastID)
+        callback()
     })
 
     if (!socket.recovered) {
